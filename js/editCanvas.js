fca97e7e26ebd7ae474e78833fbbad8f03f15455
rsaadrrtx <41071040+rsaadrrtx@users.noreply.github.com>
2019-08-19 21:42:30 -0500

    move image

--- js/editCanvas.js
+++ js/editCanvas.js
@@ -87,7 +87,7 @@ var logoComponent = {
 
     logo_image = new Image();
     logo_image.src = 'img/logo.png';
-    ctx.drawImage(logo_image, 100, 100);
+    ctx.drawImage(logo_image, 0, 200);
     
     
    /* var file = document.querySelector(this.properties.domId).files[0];
